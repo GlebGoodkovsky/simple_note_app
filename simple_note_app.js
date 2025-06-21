f2c11e399d832bc9a0e70c7dec373421f38d06dd
Gleb Goodkovsky <ggoodkovsky@gmail.com>
2025-06-20 17:29:06 -0700

    added delete button

--- simple_note_app.js
+++ simple_note_app.js
@@ -1,53 +1,40 @@
-//simple note app creation
-
-//let notes = [];
-
-//function addNote(text_of_my_note) {
-//  notes.push(text_of_my_note);
-//}
-
-//function listNotes() {
-//  notes.forEach(function(one_note) {
-//    console.log(one_note);
-//  });
-//}
-
-//prevous code, now Ill make new code
-
-//this is second version of the updated code
-
-//-----------------------------------------------------------------------------------------
-
-// Part 1: finding HTML elements.
-
-const noteInput = document.getElementById('note-input'); //This tells our script: Find the text box on the page from the html script
-const addButton = document.getElementById('add-button'); //This tells our script: Find the button on the page.from the html script
-const notesList = document.getElementById('notes-list'); //This tells our script: Find the empty list on the page from the html script
-
-// Part 2: Tell the button to listen for a click.
+const noteInput = document.getElementById('note-input');
+const addButton = document.getElementById('add-button');
+const notesList = document.getElementById('notes-list');
 
 addButton.addEventListener('click', function() {
 
-  // When the button is clicked, this code will be executed in order
-
-  // 1. Get the text the user typed in the box
   const userText = noteInput.value;
 
-  // 2. Create a new, empty list item
   const newNote = document.createElement('li');
 
-  // 3. Put the user's text inside the new list item
   newNote.textContent = userText;
 
-  // 4. Put the new list item onto the page (inside our <ul>)
+//new code down
+
+// Step 1: build a button in the browser's memory.
+// Right now a empty button is made, it's blank and not on the page yet.
+const deleteButton = document.createElement('button');
+
+// Step 2: write the word "Delete" on the face of the button.
+deleteButton.textContent = "Delete";
+
+// Step 3: give the button its one and only instruction.
+// this basicaly tells it: "Wait for someone to click you."
+deleteButton.addEventListener('click', function() {
+
+  // When the click happens, button deletes the note it's attached to
+  newNote.remove();
+});
+
+// Step 4: this takes our button and place it onto the end of the new note.
+newNote.appendChild(deleteButton);
+
+//new code ends here
+
   notesList.appendChild(newNote);
 
-  // 5. Clear the text box so it's empty for the next note
   noteInput.value = "";
 
 });
-
-
-
-
 
